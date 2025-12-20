cf2b849d474ef6bacc4c7a738c02792f9ccd0d7a
SEBAS-111 <sebasholalego587@gmail.com>
2025-12-19 20:47:40 -0600

    sonido
    -se agregaron los sonidos y sus funciones

--- My project/Assets/_Scripts/Cañon.cs
+++ My project/Assets/_Scripts/Cañon.cs
@@ -6,8 +6,13 @@ public class Cañon : MonoBehaviour
 {
     public static bool Bloqueado;
 
+    public AudioClip clipDisparo;
+    private GameObject SonidoDisparo;
+    private AudioSource SourceDisparo;
+
 
     [SerializeField] private GameObject BalaPrefab;
+    public GameObject ParticulasDisparo;
     private GameObject puntaCañon;
     private float rotacion;
     public AdministradorJuego administrador;
@@ -16,6 +21,8 @@ public class Cañon : MonoBehaviour
     private void Start()
     {
         puntaCañon = transform.Find("PuntaCañon").gameObject;
+        SonidoDisparo = GameObject.Find("SonidoDisparo");
+        SourceDisparo = SonidoDisparo.GetComponent<AudioSource>();
 
     }
     // Update is called once per frame
@@ -35,11 +42,17 @@ public class Cañon : MonoBehaviour
             if (AdministradorJuego.DisparosPorJuego > 0)
             {
                 GameObject temp = Instantiate(BalaPrefab, puntaCañon.transform.position, transform.rotation);
+
                 Rigidbody tempRB = temp.GetComponent<Rigidbody>();
                 SeguirCamara.objetivo = temp;
                 Vector3 direccionDisparo = transform.rotation.eulerAngles;
                 direccionDisparo.y = 90 - direccionDisparo.x;
+                Vector3 direccionParticulas = new Vector3(-90 + direccionDisparo.x, 90, 0);
+                GameObject Particulas = Instantiate
+                    (ParticulasDisparo, puntaCañon.transform.position, Quaternion.Euler(direccionParticulas), transform);
                 tempRB.linearVelocity = direccionDisparo.normalized * AdministradorJuego.VelocidadBala;
+                //SourceDisparo.PlayOneShot(clipDisparo);
+                SourceDisparo.Play();
                 Bloqueado = true;
 
                 AdministradorJuego.DisparosPorJuego--;
